3783c62e1d5ccd87e75d1cc907dd1e0bcbcd66ca
Volodymyr-Levchenko <dante70850@gmail.com>
2014-08-19 18:36:30 +0300

    added database and shit

---
+++ Schedule_designer/Schedule_designer/Controllers/IndexController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Web.Http;
+using Schedule_designer.Models;
+using Schedule_designer.DAL;
+
+namespace Schedule_designer.Controllers
+{
+    public class IndexController : ApiController
+    {
+
+        private Repository<Schedule> scheduleRepository;
+        private Repository<activity_type> activityTypesRepository;
+        private Repository<activity_class> activityClassRepository;
+
+        public void InitRepos()
+        {
+            
+        }
+
+        public IEnumerable<activity_type> GetActivityTypes()
+        {
+            InitRepos();
+            using (var dbContext = new ScheduleEntities())
+            {
+                scheduleRepository = new Repository<Schedule>(dbContext);
+                activityTypesRepository = new Repository<activity_type>(dbContext);
+                activityClassRepository = new Repository<activity_class>(dbContext);
+
+                IEnumerable<activity_type> res = activityTypesRepository.GetAll();
+                return res;
+            }
+           
+        }
+    }
+}
---
+++ Schedule_designer/Schedule_designer/DAL/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using Schedule_designer.Models;
+
+namespace Schedule_designer.DAL
+{
+    public class Repository<T>:IRepository<T> where T: class
+    {
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext dContext)
+        {
+            _dbSet = dContext.Set<T>();
+        }
+
+        public void Insert(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public IQueryable<T> SearchFor(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+    }
+}
